be6e504764521aab52096c927680cafb2f9b80f6
Rajat <rajat19081996kulkarni@gmail.com>
2022-04-21 17:30:04 +0530

    first commit

---
+++ Account/src/main/java/com/application/controller/Account_ServiceController.java
@@ -0,0 +1,32 @@
+package com.application.controller;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.msf.MSFrameworkCore;
+
+@RestController
+public class Account_ServiceController {
+
+	@Autowired
+	private MSFrameworkCore framework;
+
+	@RequestMapping(value = "/account", method = RequestMethod.GET, produces = "application/json")
+	public ResponseEntity<String> getAccount_Service(@PathVariable Map<String, String> pathParams,
+			@RequestParam Map<String, String> requestParams, HttpServletRequest request) throws Exception {
+		ResponseEntity<String> response = null;
+
+		response = framework.init(requestParams, pathParams, "Account_Service", request, null).invoke();
+		return response;
+	}
+}
+
